9520f13d167898ab2189e1fa05042d4ffcc88dc6
IegorIgnatii <egor.ignatiy@gmail.com>
2024-07-02 17:21:33 +0300

    go

--- js/task-2.js
+++ js/task-2.js
@@ -1,52 +1,21 @@
-// Створи клас Storage, який створюватиме об'єкти для управління складом товарів. Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного об'єкта в приватну властивість items.
+class Storage {
+  #items;
 
-// Оголоси наступні методи класу:
+  constructor(items) {
+    this.#items = items;
+  }
 
-// getItems() — повертає масив поточних товарів у приватній властивості items.
-// addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів у приватну властивість items об'єкта.
-// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
+  getItems() {
+    return this.#items;
+  }
 
-// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+  addItem(newItem) {
+    return this.#items.push(newItem);
+  }
 
-
-// class Client {
-//   #login;
-//   #email;
-
-//   constructor(email, login) {
-//     this.#login = login;
-//     this.#email = email;
-//   }
-
-//   get email() {
-//     return this.#email;
-//   }
-
-//   set email(newEmail) {
-//     this.#email = newEmail;
-//   }
-
-//   get login() {
-//     return this.#login;
-//   }
-
-//   set login(newLogin) {
-//     this.#login = newLogin;
-//   }
-// }
-
-// const client = new Client("mango@gmail.com", "Mango777");
-// client.login = "Mango333";
-
-// console.log(client.login);
-
-
-
-
-class Storage(){
-    getItems()
-        addItem(newItem)
-        removeItem(itemToRemove)
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
 }
 
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
