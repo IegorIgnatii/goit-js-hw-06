a3a1e65ca47079f9756d8e927d7ce85b0956a335
IegorIgnatii <egor.ignatiy@gmail.com>
2024-07-02 16:42:07 +0300

    1

--- js/task-2.js
+++ js/task-2.js
@@ -8,6 +8,47 @@
 
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
+
+// class Client {
+//   #login;
+//   #email;
+
+//   constructor(email, login) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+
+//   get login() {
+//     return this.#login;
+//   }
+
+//   set login(newLogin) {
+//     this.#login = newLogin;
+//   }
+// }
+
+// const client = new Client("mango@gmail.com", "Mango777");
+// client.login = "Mango333";
+
+// console.log(client.login);
+
+
+
+
+class Storage(){
+    getItems()
+        addItem(newItem)
+        removeItem(itemToRemove)
+}
+
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
